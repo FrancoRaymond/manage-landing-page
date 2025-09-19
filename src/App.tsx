ad1d5ee7006ae3d1c1acc0112e146b37f3fcadd3
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-09-19 18:58:20 +0200

    added home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar"
+import Home from "./components/Home"
 function App() {
 
   return (
-    <div className="bg-gray-100 w-full">
+    <div className="w-full">
       <Navbar />
-   
+      <Home />
     </div>
   )
 }
